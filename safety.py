8be95664b1e8b6070b9a35a765721188a41b1cf3
sorakith <60889853+max17245@users.noreply.github.com>
2025-09-01 13:59:54 +0700

    Update safety.py

--- safety.py
+++ safety.py
@@ -16,3 +16,15 @@ def get_item(item_id: int, q: Optional[str] = None):
         "full_url": f"https://api.myproject.com/items/{item_id}?q={q}" if q else f"https://api.myproject.com/items/{item_id}"
     }
 ###
+
+paths:
+  /orders:
+    post:
+      operationId: createCustomerOrder
+      tags: [Order]
+      description: 'Create new customer order'
+      responses: { '200': { description: 'New customer order is created', content: { application/json: { schema: { $ref: '#/components/schemas/OrderConfirmation' } } } } }
+      requestBody: { content: { application/json: { schema: { $ref: '#/components/schemas/Order' } } }, description: 'Customer order to be placed' }
+components:
+  securitySchemes:
+    bearerAuth:
